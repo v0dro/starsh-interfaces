04c718015aea9828911e3299b2a392a7bbdbc4dc
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2022-02-08 11:28:02 -0500

    add interfaces

---
+++ interfaces.cpp
@@ -0,0 +1,12 @@
+#include <starsh.h>
+#include <starsh-randtlr.h>
+#include <starsh-electrodynamics.h>
+#include <starsh-spatial.h>
+
+int main(int argc, char **argv) {
+  int ndim;
+  STARSH_kernel *s_kernel;
+  void *starsh_data;
+  STARSH_int * starsh_index;
+
+}
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source ~/.bashrc
+
+module load intel-mkl/2020.4.304/gcc-7.3.0-52gb
+
+export PKG_CONFIG_PATH=$HOME/gitrepos/hicma-x-dev/stars-h/build/installdir/lib/pkgconfig:$PKG_CONFIG_PATH
+
+BLAS_INCLUDE="-DMKL_LP64 -I${MKLROOT}/include -DUSE_MKL"
+BLAS_LIB="-L${MKLROOT}/lib/intel64 -Wl,--no-as-needed -lmkl_intel_lp64 -lmkl_gnu_thread -lmkl_core -lgomp -lpthread -lm -ldl"
+
+
+g++ -std=c++11 ${BLAS_INCLUDE} $(pkg-config --cflags starsh) ${BLAS_LIB} \
+    $(pkg-config --libs starsh) interfaces.cpp
+./a.out
